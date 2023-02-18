d0a29de9de80de53da6913c301986f1790ec7bf2
jaeuk Lee <lskkhj1005@naver.com>
2023-02-19 01:28:22 +0900

    feat: react-icons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Select from './components/Select';
 import Modal from './components/Modal';
 import Input from './components/Input';
 import Button from './components/Button';
+import { MdArrowForwardIos, MdAddAlert } from 'react-icons/md'
 
 const largePrimary = () => {
   alert('버튼을 만들어보세요')
@@ -17,18 +18,19 @@ function App() {
     <>
       <div>
         <h1>Button</h1>
-        <Button click={largePrimary} wd={"200px"} hg={"60px"} bor="2px solid rgb(85, 239, 196)" bgColor="white">Large Primary Button</Button>
+        <Button click={largePrimary} wd={"200px"} hg={"60px"} bor="2px solid rgb(85, 239, 196)" bgColor="white">Large Primary Button {<MdArrowForwardIos />}</Button>
         <Button wd={"100px"} hg={"50px"} bor="2px solid rgb(85, 239, 196)" bgColor="rgb(85, 239, 196)">Medium</Button>
         <Button wd={"80px"} hg={"30px"} bor="2px solid rgb(85, 239, 196)" bgColor="rgb(85, 239, 196)">Small</Button>
       </div>
       <div>
-        <Button click={largeNegative} wd={"200px"} hg={"60px"} bor="2px solid rgb(250, 177, 160)" bgColor="white">Large Negative Button</Button>
+        <Button click={largeNegative} wd={"200px"} hg={"60px"} bor="2px solid rgb(250, 177, 160)" bgColor="white">Large Negative Button {<MdAddAlert />}</Button>
         <Button wd={"100px"} hg={"50px"} bor="2px solid rgb(250, 177, 160)" bgColor="rgb(250, 177, 160)">Medium</Button>
         <Button wd={"80px"} hg={"30px"} bor="2px solid rgb(250, 177, 160)" bgColor="rgb(250, 177, 160)">Small</Button>
       </div>
       <Input />
       <Modal />
       <Select />
+      
     </>
   );
 }
